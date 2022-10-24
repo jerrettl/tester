000103ff7c513da1a7cd7b6d7f2edbccda48178b
Jerrett Longworth <jerrett7@gmail.com>
2022-10-23 20:13:05 -0400

    Add max function

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,17 @@
 fn main() {
+    println!("The max of 3 and 5 is {}", find_max(3, 5));
+
     println!("Hello, world!");
 
     for i in 1..30 {
         println!("i = {i}");
+    }
+}
+
+fn find_max(a: i32, b: i32) -> i32 {
+    if a > b {
+        a
+    } else {
+        b
     }
 }
