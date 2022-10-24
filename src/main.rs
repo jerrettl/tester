b21ea9c2f3142bd0752be2357735fec513ecf158
Jerrett Longworth <jerrett7@gmail.com>
2022-10-23 20:13:05 -0400

    Add max function

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
 fn main() {
+    println!("The max of 3 and 5 is {}", find_max(3, 5));
+
     println!("Hello, world!");
 
     println!("This is missing quotes");
+}
+
+fn find_max(a: i32, b: i32) -> i32 {
+    if a > b {
+        a
+    } else {
+        b
+    }
 }
