3872f15a39cc39e40fb8f296307c947b76f514d4
Jerrett Longworth <jerrett7@gmail.com>
2022-10-30 18:19:32 -0400

    Add yew

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,12 @@
-fn main() {
-    println!("The max of 3 and 5 is {}", find_max(3, 5));
+use yew::prelude::*;
 
-    println!("Hello, world!");
-
-    for i in 1..30 {
-        println!("i = {i}");
+#[function_component(App)]
+fn app() -> Html {
+    html! {
+        <h1>{ "Hello World" }</h1>
     }
 }
 
-fn find_max(a: i32, b: i32) -> i32 {
-    if a > b {
-        a
-    } else {
-        b
-    }
+fn main() {
+    yew::start_app::<App>();
 }
